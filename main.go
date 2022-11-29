45622280c1664055c972c417b5868b629bed498f
Maxwell Alvord <maxwellalvord@gmail.com>
2022-11-29 14:35:47 -0800

    Update: add basic console functionality for booking site.

--- main.go
+++ main.go
@@ -1,4 +1,11 @@
 package main
 
+import "fmt"
 
-Print("Hello World")
+func main() {
+	var conferenceName = "Go Conference"
+	
+	fmt.Println("Welcome to our conference booking application")
+	fmt.Println("Get your tickets here to attend")
+
+}
