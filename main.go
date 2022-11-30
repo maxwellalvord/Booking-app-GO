dbb42ecca4a5faa8aafa1e578b748f885f9bbc32
Maxwell Alvord <maxwellalvord@gmail.com>
2022-11-30 11:43:54 -0800

    update: add bones to booking functionality in the console

--- main.go
+++ main.go
@@ -3,9 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	
-	fmt.Println("Welcome to our conference booking application")
+	// var conferenceName = "Go Conference" //dry go version below
+	conferenceName := "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("conference tickets is %T, and remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
+	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	
+	var userName string
+	var userTickets int
+	//ask user for name
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 
 }
