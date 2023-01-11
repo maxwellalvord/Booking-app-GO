aeafca78a84cc3570e922e4854ffbcbfabb447a0
Maxwell Alvord <maxwellalvord@gmail.com>
2023-01-11 13:47:28 -0800

    Update: create helper package file to start to organize the functions.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 		"fmt"
 		"strings"
+		"booking-GO/helper"
 )		
 
 
@@ -21,7 +22,7 @@ func main() {
 	for {
 		
 		firstName, lastName, email, userTickets := getUserInput()
-		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
+		isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 
 		if isValidName && isValidEmail && isValidTicketNumber {
 			
@@ -69,12 +70,6 @@ func getFirstNames() []string {
 	return firstNames
 }
 
-func validateUserInput (firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(email, "@")
-	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
-	return isValidName, isValidEmail, isValidTicketNumber
-}
 
 func getUserInput () (string, string, string, uint) {
 	var firstName string
